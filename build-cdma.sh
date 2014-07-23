68e1fa43ff99f995070bcb869bbd2f37aa018bdc
dogredwing1 <dogredwing1@gmail.com>
2014-07-22 21:23:20 -0500

    DEFCONFIG: EDITS

--- build-cdma.sh
+++ build-cdma.sh
@@ -4,14 +4,14 @@
 
 # Kernel Version
 BASE_VER=".KANGAROO"
-VER=".M7.wlv.wls.v110"
+VER=".M7.v116"
 VER=$BASE_VER$VER
 
 # AK Variables
 export LOCALVERSION=""`echo $VER`
 
 # toolchain
-export CROSS_COMPILE=${HOME}/Toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.06/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=${HOME}/linaro_toolchains_2014-2014.07/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf--
 
 # make .config
 make m7wls_defconfig
